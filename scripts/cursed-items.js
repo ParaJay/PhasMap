b4f6796566548dafdcfca25f2a74c85f11452ce1
ParaJay <calumjhunter@gmail.com>
2023-02-28 20:12:17 +0000

    added monkey paw

--- scripts/cursed-items.js
+++ scripts/cursed-items.js
@@ -1,6 +1,6 @@
 import { initInfoState } from "./utils.js";
 
-const cursedItems = ["Haunted Mirror", "Music Box", "Ouija Board", "Summoning Circle", "Tarot Cards", "Voodoo Doll"]
+const cursedItems = ["Haunted Mirror", "Monkey Paw", "Music Box", "Ouija Board", "Summoning Circle", "Tarot Cards", "Voodoo Doll"]
 
 async function init() {
     await initInfoState(cursedItems, "cursed-items", "curseditem");
